5224f2b06db8b5c79005be1600e34425ede0a967
Hylke Bons <hello@planetpeanut.studio>
2025-08-24 15:31:50 +0200

    ssh config: Lower connection thresholds

--- src/ssh/objects/config.rs
+++ src/ssh/objects/config.rs
@@ -7,6 +7,7 @@
 
 use std::fmt;
 use std::path::PathBuf;
+use std::time::Duration;
 
 
 #[allow(non_snake_case)]
@@ -15,9 +16,13 @@ pub struct SshConfig {
     // Docs: https://man.openbsd.org/ssh_config
 
     pub BatchMode: bool,
+    pub ConnectionAttempts: u32,
+    pub ConnectTimeout: Duration,
     pub IdentitiesOnly: bool,
     pub IdentityFile: PathBuf,
     pub PasswordAuthentication: bool,
+    pub ServerAliveCountMax: u32,
+    pub ServerAliveInterval: Duration,
     pub StrictHostKeyChecking: bool,
     pub UserKnownHostsFile: PathBuf,
 }
@@ -30,9 +35,13 @@ impl Default for SshConfig {
 
         SshConfig {
             BatchMode: true,
+            ConnectionAttempts: 2,
+            ConnectTimeout: Duration::from_secs(4),
             IdentitiesOnly: true,
             IdentityFile: PathBuf::new(),
             PasswordAuthentication: false,
+            ServerAliveCountMax: 2,
+            ServerAliveInterval: Duration::from_secs(4),
             StrictHostKeyChecking: true,
             UserKnownHostsFile: PathBuf::new(),
         }
@@ -44,9 +53,13 @@ impl fmt::Display for SshConfig {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let options = vec![
             format!("-o BatchMode={}", format_bool(self.BatchMode)),
+            format!("-o ConnectionAttempts={}", self.ConnectionAttempts),
+            format!("-o ConnectTimeout={}", self.ConnectTimeout.as_secs()),
             format!("-o IdentitiesOnly={}", format_bool(self.IdentitiesOnly)),
             format!("-o IdentityFile={}", self.IdentityFile.to_string_lossy()),
             format!("-o PasswordAuthentication={}", format_bool(self.PasswordAuthentication)),
+            format!("-o ServerAliveCountMax={}", self.ServerAliveCountMax),
+            format!("-o ServerAliveInterval={}", self.ServerAliveInterval.as_secs()),
             format!("-o StrictHostKeyChecking={}", format_bool(self.StrictHostKeyChecking)),
             format!("-o UserKnownHostsFile={}", self.UserKnownHostsFile.to_string_lossy()),
         ];
