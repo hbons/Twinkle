df364abbcdf118c11309448952f45c9cea1533a7
Hylke Bons <hello@planetpeanut.studio>
2025-08-28 22:01:50 +0200

    script: Don't push release immediately

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 if [ "$1" = "" ]; then
-    echo "No version number specified. Usage: ./create-version.sh VERSION_NUMBER"
+    echo "No version number specified. Usage: ./create-release.sh VERSION_NUMBER"
 else
-    ./bump-version.sh $1
     cargo audit
     cargo test
     cargo build --release
     git tag $1
-    git push --tags
+    echo "Tagged $1. Run 'git push --tags' to publish."
 fi
