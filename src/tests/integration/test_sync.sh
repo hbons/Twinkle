901a8e6f02f878fa34365c4129ab0b62506f31eb
Hylke Bons <hello@planetpeanut.studio>
2026-07-17 14:04:47 +0200

    tests sync: Tweaks and todos

--- src/tests/integration/test_sync.sh
+++ src/tests/integration/test_sync.sh
@@ -5,8 +5,8 @@ source ./common/config.sh
 source ./common/install_keys.sh
 
 REPO_NAME=test_sync_$TAG
-REPO_NAME_1=test_sync_{$TAG}1
-REPO_NAME_2=test_sync_{$TAG}2
+REPO_NAME_1=test_sync_"$TAG"_1
+REPO_NAME_2=test_sync_"$TAG"_2
 
 gh repo create \
     $REPO_NAME \
@@ -20,7 +20,7 @@ twinkle clone \
 
 mv $REPO_NAME $REPO_NAME_1
 cd $REPO_NAME_1
-timeout 15s twinkle sync || true  # --once
+# timeout 15s twinkle sync || true  # --once
 cd ..
 
 DEBUG=1 twinkle clone \
@@ -29,7 +29,6 @@ DEBUG=1 twinkle clone \
 
 mv $REPO_NAME $REPO_NAME_2
 
-
 cd $REPO_NAME_2
 echo " ...a conflict!" >> README.md
 timeout 15s twinkle sync || true  # --once
@@ -37,7 +36,7 @@ cd ..
 
 cd $REPO_NAME_1
 echo " Let's create..." >> README.md
-timeout 15s twinkle sync || true  # --once
+timeout 45s twinkle sync || true  # --once
 
 echo "--- README.md ---"
 cat README.md
@@ -51,6 +50,9 @@ echo "---"
 test -f README.md
 test -f "README (A).md"
 test -f "README (B).md"
+
+
+# TODO: Test all conflict paths: AA, UU, AU, UA, DU, UD, DD, XX, QQ
 
 
 # TODO: Doesn't work...
