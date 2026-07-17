a717e7f9f7892d1ff98276caead1f42d522e616f
Hylke Bons <hello@planetpeanut.studio>
2026-07-17 14:01:22 +0200

    tests: Install+delete keys by id

--- src/tests/integration/common/install_keys.sh
+++ src/tests/integration/common/install_keys.sh
@@ -2,21 +2,43 @@
 
 set -euo pipefail
 
+echo "--- INSTALL KEYS ---"
+
+KEY_TITLE="Integration Test $TAG"
+
+echo "KEY_FILE: $KEY_FILE"
+echo "KEY_TITLE: $KEY_TITLE"
+
+
 ssh-keygen \
     -f $KEY_FILE \
     -t ed25519 \
     -P ""
 
+eval "$(ssh-agent -s)"
+ssh-add $KEY_FILE
+ssh-add -l
+
+
 gh ssh-key add \
     --type authentication \
-    --title "Twinkle Integration Test" \
+    --title "$KEY_TITLE" \
     $KEY_FILE.pub
 
 gh ssh-key add \
     --type signing \
-    --title "Twinkle Integration Test" \
+    --title "$KEY_TITLE" \
     $KEY_FILE.pub
 
-eval "$(ssh-agent -s)"
-ssh-add $KEY_FILE
-ssh-add -l
+
+export KEY_ID=$(
+    gh api user/keys \
+        --jq ".[] | select(.title == \"$KEY_TITLE\") | .id"
+)
+
+export SIGNING_KEY_ID=$(
+    gh api user/ssh_signing_keys \
+        --jq ".[] | select(.title == \"$KEY_TITLE\") | .id"
+)
+
+echo "--- END INSTALL KEYS ---"
