5456a66ddaabeb86d2fb46af504c148d756e46b3
test <hello@planetpeanut.studio>
2026-07-28 14:47:00 +0200

    tests integration: Run check after init and clone

--- src/tests/integration/test_clone.sh
+++ src/tests/integration/test_clone.sh
@@ -23,5 +23,7 @@ touch NEW_FILE2
 timeout 20s twinkle sync || true  # --once
 source ../common/test_synced.sh
 
+twinkle check
+
 cd ..
 source ./common/cleanup.sh
--- src/tests/integration/test_init.sh
+++ src/tests/integration/test_init.sh
@@ -19,4 +19,6 @@ twinkle init \
 timeout 20s twinkle sync || true  # --once
 source ./common/test_synced.sh
 
+twinke check
+
 source ./common/cleanup.sh
