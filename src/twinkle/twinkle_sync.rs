d9a64950ef39b9f7773ed4e6f4f80e0777099c47
test <hello@planetpeanut.studio>
2026-08-02 21:36:20 +0200

    git sync: Always get remote+branch from repo

--- src/twinkle/twinkle_sync.rs
+++ src/twinkle/twinkle_sync.rs
@@ -213,7 +213,9 @@ pub fn twinkle_sync_up(repo: &mut TwinkleRepository) -> Result<(), Box<dyn Error
         let status = repo.git.status()?; // TODO: status_staged()
 
         let branch = repo.git.branch_show_current()?;
-        let remote = repo.remote(&branch);
+        let remote = repo.git.config_get(&format!("branch.{branch}.remote"))
+            .ok_or("Missing branch.*.remote")?
+            .stdout;
 
         if let Some(message) = twinkle_pretty_commit_message(&status) {
             let user = repo.user().ok_or("User not set")?;
@@ -279,7 +281,9 @@ pub fn twinkle_sync_up_delay(attempt: u64) -> Duration {
 
 pub fn twinkle_sync_down(repo: &mut TwinkleRepository) -> Result<(), Box<dyn Error>> {
     let branch = repo.git.branch_show_current()?;
-    let remote = repo.remote(&branch);
+    let remote = repo.git.config_get(&format!("branch.{branch}.remote"))
+        .ok_or("Missing branch.*.remote")?
+        .stdout;
 
     repo.git.fetch(&remote, &branch)?;
 
