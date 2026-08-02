d05700583212564483dba7115d75c31c25b8b299
Hylke Bons <hello@planetpeanut.studio>
2026-08-02 15:18:49 +0200

    ci integration: Also test on macOS

--- src/tests/integration/common/config.sh
+++ src/tests/integration/common/config.sh
@@ -2,5 +2,5 @@
 
 export ACCOUNT=sparkleshare-org
 export KEY_FILE=~/.ssh/twinkle_ed25519
-export TAG="${GITHUB_REF_NAME}_${GITHUB_SHA}"
+export TAG="${GITHUB_REF_NAME}_${GITHUB_SHA}_${RUNNER_ARCH}"
 export DEBUG=1
