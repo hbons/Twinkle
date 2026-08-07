cf4c99f7c2e2331fee50ad9227fa58537f4ea950
test <hello@planetpeanut.studio>
2026-08-07 22:57:19 +0200

    main: Use cli mode by default

--- src/app.rs
+++ src/app.rs
@@ -114,12 +114,6 @@ pub fn app_runs_as_root() -> bool {
 }
 
 
-pub fn app_runs_in_terminal() -> bool {
-    env::var("TERM").is_ok() || // TODO: Fragile, find a better way
-    env::var("GITHUB_ACTIONS").is_ok()
-}
-
-
 pub fn app_version() -> String {
     let s = format!("{} {}",
         env!("CARGO_PKG_NAME"),
