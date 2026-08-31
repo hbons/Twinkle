847bbbd75161eeb4bf3e5483fb9ecfbbfa4847c8
Hylke Bons <hello@planetpeanut.studio>
2026-08-31 15:21:50 +0200

    tests mod: Sort

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -8,6 +8,22 @@
 #![allow(clippy::field_reassign_with_default)]
 #![allow(clippy::unwrap_used)]
 
+
+#[cfg(test)]
+mod core {
+    mod test_default_dir_name; // TODO: test_core_?
+    mod test_default;
+    mod test_object_repository;
+    mod test_pretty_bool;
+    mod test_pretty_commit_message;
+    mod test_pretty_datetime;
+    mod test_pretty_dir;
+    mod test_resolve_paths;
+    mod test_unique_dir;
+    mod test_settings_url_for_host;
+    mod test_sync;
+}
+
 #[cfg(test)]
 mod ssh {
     mod test_ssh_keygen;
@@ -28,19 +44,4 @@ mod git {
     mod test_git_object_merge_status;
     mod test_git_object_user;
     mod test_git_object_version;
-}
-
-#[cfg(test)]
-mod core {
-    mod test_default_dir_name;
-    mod test_default;
-    mod test_object_repository;
-    mod test_pretty_bool;
-    mod test_pretty_commit_message;
-    mod test_pretty_datetime;
-    mod test_pretty_dir;
-    mod test_resolve_paths;
-    mod test_unique_dir;
-    mod test_settings_url_for_host;
-    mod test_sync;
 }
