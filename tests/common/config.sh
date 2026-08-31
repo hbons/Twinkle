b0f4fdb6a93920dff09dfc5fc17d5c4d37f9d8a7
Hylke Bons <hello@planetpeanut.studio>
2026-08-31 17:45:11 +0200

    integration tests: Print dependency versions

--- tests/common/config.sh
+++ tests/common/config.sh
@@ -4,3 +4,7 @@ export ACCOUNT=sparkleshare-org
 export KEY_FILE=~/.ssh/ed25519
 export TAG="${GITHUB_REF_NAME}_${GITHUB_SHA}_${RUNNER_ARCH}"
 export DEBUG=1
+
+ssh -V
+git --version
+git lfs --version
