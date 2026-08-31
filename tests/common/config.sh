df6bdc5928fdfb0d7cfe428a96aa947b36f5f876
Hylke Bons <hello@planetpeanut.studio>
2026-08-31 11:26:53 +0200

    Move integration tests to tests/

--- tests/common/config.sh
+++ tests/common/config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export ACCOUNT=sparkleshare-org
-export KEY_FILE=~/.ssh/twinkle_ed25519
+export KEY_FILE=~/.ssh/ed25519
 export TAG="${GITHUB_REF_NAME}_${GITHUB_SHA}_${RUNNER_ARCH}"
 export DEBUG=1
