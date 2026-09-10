ad5c241d3a48b3314288ba2dcce5ac241e9450b0
Hylke Bons <hello@planetpeanut.studio>
2026-09-10 10:31:35 +0200

    tests: Add pretty_repo_change

---
+++ src/tests/core/test_pretty_repo_change.rs
@@ -0,0 +1,42 @@
+//   Twinkle, automatic syncing with Git
+//   Copyright (C) 2025  Hylke Bons (hello@planetpeanut.studio)
+//
+//   This program is free software: you can redistribute it and/or modify it
+//   under the terms of the GNU General Public License v3 or any later version.
+
+
+use crate::core::pretty::{
+    format_file_status,
+    format_repo_change,
+};
+
+use crate::git::objects::change::GitChange;
+use crate::git::objects::status::GitFileStatus;
+use crate::ssh::objects::url::SshUrl;
+
+
+#[test]
+fn test_pretty_repo_change() {
+    let change =
+        GitChange {
+            status_x: Some(GitFileStatus::Added),
+            status_y: None,
+            path: "test.txt".into(),
+        };
+
+    let letter = format_file_status(&change.status_x.clone().unwrap());
+
+    let url = "ssh://git@github.com:22/hbons/Twinkle".parse::<SshUrl>().unwrap();
+    let branch = "main".to_string();
+    let hash = "97b9e";
+
+    let s = format_repo_change(&url, &branch, hash, &change).unwrap();
+
+    assert_eq!(s, format!("github.com:hbons/Twinkle | {hash} | {letter} `test.txt`"));
+
+
+    let change = GitChange::default();
+    let r = format_repo_change(&url, &branch, hash, &change);
+
+    assert!(r.is_none());
+}
